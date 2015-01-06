5602144194c23df52d75bd1da21c336d87da94d0
a.padmakumar <a.padmakumar@BDC6-L-DLPZ5Z1.GDNINDIA.COM>
2015-01-06 16:56:14 +0530

    V1.0

--- L2-Support/PRD/ENH000005/SampleTest2_PRD.js
+++ L2-Support/PRD/ENH000005/SampleTest2_PRD.js
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest_L2_2 PRD {
   public static void main(String[] args) {
-    System.out.println("GitHub is a VSS PRD");
+    System.out.println("GitHub is a VSS PRODUCTION");
   }
 } 
